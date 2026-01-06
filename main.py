86ed4512d612c86551b7b2f5471d3eaad4c9a69c
Sharath P <chandranpsharath@gmail.com>
2026-01-06 23:42:39 +0530

    feat: path params

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-
 BOOKS = [
-    {
-        "id": 1,
-        "title": "Book1",
-        "price": 200,
-    }
+    {"id": 1, "title": "Title 1", "author": "Author 1", "category": "Science"},
+    {"id": 2, "title": "Title 2", "author": "Author 2", "category": "Science"},
+    {"id": 3, "title": "Title 3", "author": "Author 3", "category": "History"},
+    {"id": 4, "title": "Title 4", "author": "Author 4", "category": "Math"},
+    {"id": 5, "title": "Title 5", "author": "Author 5", "category": "Math"},
 ]
 
 
-@app.get("/")
-def first_api():
+@app.get("/books")
+def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_id}")
+def read_one_book(book_id: int):
+    book = next((b for b in BOOKS if b["id"] == book_id), None)
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
